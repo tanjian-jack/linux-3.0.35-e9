8dfc8fb2f35885f426271d9b90e97bb94ef89875
ubuntu <ubuntu@ubuntu.(none)>
2016-06-07 00:33:24 +0800

    add linux kernel.

---
+++ arch/arm/mach-mx6/tq_e9.h
@@ -0,0 +1,7 @@
+#ifndef _TQ_E9_H
+#define _TQ_E9_H
+int mx6q_sabresd_fec_phy_init(struct phy_device *phydev);
+int tq_tsc_exit_hw(void);
+int tq_tsc_init_hw(void);
+int tq_tsc_state_hw(void);
+#endif
